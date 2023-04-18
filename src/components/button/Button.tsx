374bc2c7a6e0d7796f1dcc4550f513f33f61260b
emirrtopaloglu <emirrttopaloglu@gmail.com>
2023-04-18 22:51:02 +0300

    first commit

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+    background-color: #000;
+    color: #fff;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 16px;
+    font-weight: 600;
+    transition: all 0.2s ease-in-out;
+
+`;
